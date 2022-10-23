54b9fea0e2023c08944cee1baa134fd1320d347f
AminRezgui <amin.rezgui@softt365.com>
2022-10-23 11:32:21 +0100

    searchinput done

--- App.tsx
+++ App.tsx
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import SearchInput from "./app/components/SearchInput";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <SearchInput
+        placeholder="Search"
+        onChangeText={(text) => console.log(text)}
+      />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#191919",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
